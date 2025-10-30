66810546dd06abfce475639ad26a154673b9e713
Oleh Solomka <olehsolomka.dev@gmail.com>
2025-10-30 02:00:26 +0100

    fix env

--- src/infrastructure/config/environments.ts
+++ src/infrastructure/config/environments.ts
@@ -1,19 +1,20 @@
-import z from 'zod'
+import { ENVIRONMENTS } from '@app/libs'
+import z, { optional } from 'zod'
 
 export const environmentsSchema = z.object({
   PORT: z.coerce.number().default(3000),
-  REDIS_URL: z.string().default('redis://localhost:6379'),
+  REDIS_URL: z.string(),
   DATABASE_URL: z.string(),
   BOT_TOKEN: z.string(),
-  BOT_TOKEN_TEST: z.string().default(''),
+  BOT_TOKEN_TEST: z.string().optional(),
   MAINTAINER_CHAT_ID: z.string(),
   TIME_ZONE: z.string().default('Europe/Kyiv'),
   RAILWAY_PUBLIC_DOMAIN: z.string().default('localhost'),
   LOG_LEVEL: z.enum(['debug', 'info', 'warn', 'error', 'trace']).default('debug'),
   LOG_LEVEL_HTTP: z.enum(['debug', 'info', 'warn', 'error', 'trace']).default('debug'),
-  NODE_ENV: z.enum(['development', 'production']).default('development'),
+  NODE_ENV: z.enum([ENVIRONMENTS.DEV, ENVIRONMENTS.PRODUCTION]).default(ENVIRONMENTS.DEV),
   STUDIO_ID: z.string(),
-  STUDIO_ID_TEST: z.string().default(''),
+  STUDIO_ID_TEST: z.string().optional(),
   GOOGLE_PLACE_ID: z.string(),
 })
 
