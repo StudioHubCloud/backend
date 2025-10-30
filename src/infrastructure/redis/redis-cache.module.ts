4e5d5e87dcfb35843195ea84a1ab1fd956baf32b
Oleh Solomka <olehsolomka.dev@gmail.com>
2025-10-30 02:10:35 +0100

    add logs

--- src/infrastructure/redis/redis-cache.module.ts
+++ src/infrastructure/redis/redis-cache.module.ts
@@ -4,7 +4,7 @@ import { redisStore } from 'cache-manager-ioredis-yet'
 import { Global, Module } from '@nestjs/common'
 import { TypedConfigService } from '../config'
 import { RedisCacheService } from './redis-cache.service'
-import { CACHE, ENVIRONMENTS } from '@app/libs'
+import { CACHE } from '@app/libs'
 
 @Global()
 @Module({
@@ -12,11 +12,17 @@ import { CACHE, ENVIRONMENTS } from '@app/libs'
     CacheModule.registerAsync<RedisOptions>({
       isGlobal: true,
       inject: [TypedConfigService],
-      useFactory: async (configService: TypedConfigService) => ({
-        store: redisStore,
-        url: configService.get('REDIS_URL'),
-        ttl: CACHE.DEFAULT_TTL,
-      }),
+      useFactory: async (configService: TypedConfigService) => {
+
+         const redisUrl = configService.get('REDIS_URL')
+        console.log('🔴 Connecting to Redis URL:', redisUrl)
+
+        return {
+          store: redisStore,
+          url: redisUrl,
+          ttl: CACHE.DEFAULT_TTL,
+        }
+      },
     }),
   ],
   providers: [RedisCacheService],
